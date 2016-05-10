e8ccf8cab2d1da2692699e5e4bd14238daa44d59
myungpyo,shim <myungpyo.shim@worksmobile.com>
2016-05-10 12:12:44 +0900

    add playWithMockApi to flatMap playground

--- app/src/test/java/com/smp/rxplayround/sample/Play30_TransformOperator_FlatMap.java
+++ app/src/test/java/com/smp/rxplayround/sample/Play30_TransformOperator_FlatMap.java
@@ -5,10 +5,14 @@ import com.smp.rxplayround.support.Utils;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.extern.slf4j.Slf4j;
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
+import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
@@ -65,6 +69,110 @@ public class Play30_TransformOperator_FlatMap extends BasePlayground {
                 }
             });
 
+    }
 
+    @Test
+    public void playWithMockApi() throws Exception {
+
+
+        List<String> urlList = new ArrayList<>();
+        urlList.add("http://test1.com");
+        urlList.add("http://test2.com");
+        urlList.add("http://test3.com");
+
+        Observable.just(urlList)
+                .flatMap(new Func1<List<String>, Observable<DownloadResult>>() {
+                    @Override
+                    public Observable<DownloadResult> call(final List<String> urlList) {
+
+                        return Observable.create(new Observable.OnSubscribe<DownloadResult>() {
+                            @Override
+                            public void call(final Subscriber<? super DownloadResult> subscriber) {
+                                subscriber.onStart();
+
+                                for (String url : urlList) {
+                                    MockRetrofitApi.request(url, true).subscribe(new Action1<DownloadResult>() {
+                                        @Override
+                                        public void call(DownloadResult downloadResult) {
+                                            log.debug("onSubscribed : {}", downloadResult);
+                                            subscriber.onNext(downloadResult);
+                                        }
+                                    });
+                                }
+
+                                //Uncomment below snippet to test partially failed
+//                                MockRetrofitApi.request("http://fail.test1.com", false).subscribe(new Action1<DownloadResult>() {
+//                                    @Override
+//                                    public void call(DownloadResult downloadResult) {
+//                                        subscriber.onNext(downloadResult);
+//                                    }
+//                                });
+
+                                subscriber.onCompleted();
+                            }
+                        });
+                    }
+                })
+                .all(new Func1<DownloadResult, Boolean>() {
+                    @Override
+                    public Boolean call(DownloadResult downloadResult) {
+                        return downloadResult.statusCode >= 200 && downloadResult.statusCode < 300;
+                    }
+                })
+                .subscribe(new Observer<Boolean>() {
+                    @Override
+                    public void onCompleted() {
+                        log.debug("onComplete");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        log.debug("onError : {}", e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Boolean aBoolean) {
+                        log.debug("onNext : success : {}", aBoolean);
+                    }
+                });
+
+
+
+    }
+
+    static class DownloadModel {
+        private List<String> urlList;
+
+        public DownloadModel(List<String> urlList) {
+            this.urlList = urlList;
+        }
+    }
+
+    static class DownloadResult {
+        int statusCode;
+        String message;
+
+        public DownloadResult(int statusCode, String message) {
+            this.statusCode = statusCode;
+            this.message = message;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(statusCode) + " " + message;
+        }
+    }
+
+    static class MockRetrofitApi {
+
+        public static Observable<DownloadResult> request(final String url, final boolean success) {
+            return Observable.create(new Observable.OnSubscribe<DownloadResult>() {
+                @Override
+                public void call(Subscriber<? super DownloadResult> subscriber) {
+                    subscriber.onStart();
+                    subscriber.onNext(new DownloadResult(success ? 200 : 500, url + " : OK"));
+                }
+            });
+        }
     }
 }
